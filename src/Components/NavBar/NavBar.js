eff2f270f39e4942cb5622916e8b3d224be7a879
Dan Minahan <daniel.minahan9@gmail.com>
2023-09-12 22:35:56 -0400

    added pages and udpated colors

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Logo from "../Images/Logo.png";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../Styles/Styles.css";
+
+export function NavBar() {
+  const ColoredLine = () => (
+    <div className="d-flex align-items-center">
+      <div
+        className="vr"
+        style={{
+          color: "#e42e31",
+          height: "2rem",
+        }}
+      ></div>
+    </div>
+  );
+
+  return (
+    <Navbar id="navbar" className="sticky-top">
+      <Container className="hstack gap-3">
+        <Navbar.Brand href="/">
+          <img alt="" src={Logo} id="logo"></img>
+        </Navbar.Brand>
+        <Nav className="ms-auto fs-6 fw-bold">
+          <Nav.Link href="/" id="link">
+            HOME
+          </Nav.Link>
+        </Nav>
+        <ColoredLine />
+        <Nav className="fs-6 fw-bold">
+          <Nav.Link href="/about" id="link">
+            ABOUT
+          </Nav.Link>
+        </Nav>
+        <ColoredLine />
+        <Nav className="fs-6 fw-bold">
+          <Nav.Link href="/services" id="link">
+            EVENTS
+          </Nav.Link>
+        </Nav>
+        <ColoredLine />
+        <Nav className="fs-6 fw-bold">
+          <Nav.Link href="/contact" id="link">
+            CONTACT US
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavBar } from "../NavBar/NavBar";
+import { Footer } from "../Footer/Footer";
+// import Wordcloud from "../Images/WordCloud.png";
+// import BUAIL from "../Images/BUAIL.PNG";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../Styles/Styles.css";
+
+// import { Container, Card, Button } from "react-bootstrap";
+
+export function Services() {
+  return (
+    <>
+      {/* <MetaTags>
+        <title>Home</title>
+        <meta
+          name="Home"
+          content="This Website is designed to show Dan Minahan's Resume and Portfolio. Enjoy!"
+        />
+        <meta property="og:title" content="Home" />
+        <meta property="og:image" content="../Images/Headshot.png" />
+      </MetaTags> */}
+
+      <NavBar />
+      <div id="container"></div>
+
+      <Footer />
+    </>
+  );
+}
